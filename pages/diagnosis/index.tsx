48ffa24eda729b9207d202aaaa5ecc7e3f0c7729
hotter6163 <95465875+hotter6163@users.noreply.github.com>
2022-06-08 19:20:21 +0900

    診断ページの機能面を作成

--- pages/diagnosis/index.tsx
+++ pages/diagnosis/index.tsx
@@ -1,15 +1,74 @@
-import { useState } from 'react'
-import type { NextPage } from 'next'
-import Router from 'next/router'
+import type { GetStaticProps, NextPage } from 'next'
 import {
   Button,
   Typography
 } from '@mui/material'
+import * as fs from 'fs'
+import * as path from 'path'
+import { useEffect, useState } from 'react'
 
-const Diagnosis:NextPage = () => {
-  const handleClickResult = () => {
-    Router.push('/diagnosis/results')
+
+type QuestionType = {
+  id: string
+  question: string
+}
+type PageQuestionsType = {
+  title: string
+  questions: QuestionType[]
+}
+type Props = {
+  ids: string[]
+  questions: PageQuestionsType[]
+}
+type AnswersType = { [key: string]: boolean | null }
+
+const Diagnosis: NextPage<Props> = ({ ids, questions }) => {
+  const [pageNum, setPageNum] = useState<0 | 1>(0)
+  const [answers, setAnswers] = useState<AnswersType>({})
+
+  const handleAnswerClicked = (id: string, answer: boolean) => {
+    setAnswers((prevValue) => {
+      const clone: AnswersType = Object.assign({}, prevValue)
+      clone[id] = answer
+      return clone
+    })
   }
+
+  useEffect(()=> {
+    console.log(answers)
+  }, [answers])
+  const questionsComponent = (
+    <section className="mt-3 bg-white p-3">
+      <Typography
+        variant="h2"
+        component="h2"
+        className="text-xl font-medium mb-4"
+      >
+        {questions[pageNum]["title"]}
+      </Typography>
+      {questions[pageNum]["questions"].map((question, index) => (
+        <div key={question.id} className="mb-3">
+          <div className="pb-1 border-b-2 border-yellow-200 text-left">
+            <Typography
+              variant="inherit"
+              component="p"
+              className="text-xl font-medium"
+            >
+              {`Q${index+1}：${question.question}`}
+            </Typography>
+          </div>
+          <div className="flex justify-around">
+            <Button className="first" onClick={() => handleAnswerClicked(question.id, true)}>
+              はい
+            </Button>
+            <Button className="last" onClick={() => handleAnswerClicked(question.id, false)}>
+              いいえ
+            </Button>
+          </div>
+        </div>
+      ))}
+    </section>
+  )
 
   return (
     <main>
@@ -21,20 +80,32 @@ const Diagnosis:NextPage = () => {
       >
         Apexキャラ診断
       </Typography>
-      <Typography
-        variant="h2"
-        component="h2"
-        gutterBottom
-        className="text-xl font-normal"
-      >
-        質問に答えてください。
-      </Typography>
-      <Button variant="contained" className="bg-blue-500" size="large" onClick={handleClickResult}>
-        診断結果を見る
+      {questionsComponent}
+      <Button onClick={pageNum === 1 ? () => setPageNum(0) : () => setPageNum(1)}>
+        次のページへ
       </Button>
     </main>
   )
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const jsonPath = path.join(process.cwd(), 'data', 'questions.json')
+  const jsonText = fs.readFileSync(jsonPath, 'utf-8')
+  const questions = JSON.parse(jsonText) as PageQuestionsType[]
+
+  const ids: string[] = []
+  questions.forEach(row => {
+    row.questions.forEach(cell => {
+      ids.push(cell.id)
+    })
+  })
+
+  return {
+    props: {
+      ids,
+      questions
+    }
+  }
+}
 
 export default Diagnosis
